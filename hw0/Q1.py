f764f97a1bafbbe28370c7664b07f3bc7bcaa4d1
b03902090 <b03902090@linux1.csie.ntu.edu.tw>
2017-02-23 21:38:45 +0800

    hw0_2nd

--- hw0/Q1.py
+++ hw0/Q1.py
@@ -3,4 +3,5 @@ import sys
 x = np.loadtxt(sys.argv[1],dtype='int',delimiter=',')
 y = np.loadtxt(sys.argv[2],dtype='int',delimiter=',')
 z = np.matmul(x,y)
-np.savetxt('ans_one.txt',np.sort(z),fmt='%d')
+out = np.reshape(z,-1)
+np.savetxt('ans_one.txt',np.sort(out),fmt='%d')
