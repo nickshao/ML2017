46edc0a7c37a3d26a0a55991e1f869cfced230cb
b03902090 <b03902090@linux1.csie.ntu.edu.tw>
2017-02-23 22:55:52 +0800

    fixed ndmin=2

--- hw0/Q1.py
+++ hw0/Q1.py
@@ -1,7 +1,7 @@
 import numpy as np
 import sys
-x = np.loadtxt(sys.argv[1],dtype='int',delimiter=',')
-y = np.loadtxt(sys.argv[2],dtype='int',delimiter=',')
+x = np.loadtxt(sys.argv[1],dtype='int',delimiter=',', ndmin=2)
+y = np.loadtxt(sys.argv[2],dtype='int',delimiter=',', ndmin=2)
 z = np.matmul(x,y)
 out = np.reshape(z,-1)
 np.savetxt('ans_one.txt',np.sort(out),fmt='%d')
